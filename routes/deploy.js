897a5303359ecd5b623e2c787a8ccd97cdc7ed09
Siddharth VP <siddharthvp@gmail.com>
2025-06-24 23:48:46 +0530

    Fix export syntax

--- routes/deploy.js
+++ routes/deploy.js
@@ -1,11 +1,5 @@
-const path = require('path');
-const { spawn } = require("child_process");
 const express = require('express');
 require('express-async-errors');
-const fs = require('fs-extra');
-const ansiHtml = require('ansi-html');
-const {logger} = require('../logger');
-const conf = require('../conf');
 
 const router = express.Router();
 
@@ -13,4 +7,4 @@ router.get('/', async (req, res) => {
 	res.redirect(308, 'https://gadget-deploy.toolforge.org/');
 });
 
-export default router;
+module.exports = router;
